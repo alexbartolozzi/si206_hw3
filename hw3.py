b1c5b2aed02245783932fc9ab85da5c0c3b888f1
Alex Bartolozzi <alexbart@umich.edu>
2024-09-19 12:47:19 -0400

    Function defs

---
+++ hw3.py
@@ -0,0 +1,18 @@
+import random
+
+class DigitalBookofAnswers:
+
+    def __init__(self, answers):
+
+
+    def __str__(self):
+
+    
+    def check_get_answer(self, question):
+
+
+    def open_book(self):
+
+    def answer_log(self):
+
+
